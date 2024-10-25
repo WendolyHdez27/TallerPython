dbdad05470f1d5d0abbb01e7c1ea3dc1f0149370
Wendoly <l22240028@smartin.tecnm.mx>
2024-10-25 15:33:02 -0600

    Primeros progrmas

---
+++ Estructurada/1. Main.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':#Inicio del main
+    print("Hola Mundo")
+    print("Hola Grupo")
+    print("Ya casi aplico examen")
---
+++ Estructurada/2. Variables.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a=int(input("Dame un numero" ))#Para ingesar datos desde consola
+    b=int(input("Dame otro numero" ))#Para ingesar datos desde consola
+    c=int(input("Con este numero terminamos" ))#Para ingesar datos desde consola
+
+    resultado = a*a+b*b+c*c
+    print(f"el resultado es {resultado}")#forma pro
+    print("el resultado es", resultado)#forma sencilla
